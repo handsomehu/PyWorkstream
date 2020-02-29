679e4699da635360e00aa7bef2ba9c13b9641c06
handsomehu <leonzh18@hotmail.com>
2020-02-29 22:39:44 +0800

    hello world

--- FluentPy/HelloWorld.py
+++ FluentPy/HelloWorld.py
@@ -1,11 +1,15 @@
 import collections
+from math import hypot
+import builtins
+from vector2d import Vector2d
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
-
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
     suits = 'spades diamonds clubs hearts'.split()
+
 
     def __init__(self):
         self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
@@ -17,7 +21,59 @@ class FrenchDeck:
         return self._cards[position]
 
 
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+class Vector:
+
+
+    def __init__(self, x = 0, y = 0):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+    def __abs__(self):
+        return hypot(self.x, self.y)
+    def __bool__(self):
+        return bool(abs(self))
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+
 beer_card = Card('7', 'diamonds')
 print(beer_card)
 deck = FrenchDeck()
 print(len(deck))
+for card in sorted(deck, key=spades_high): # doctest: +ELLIPSIS
+    print(card)
+
+colors = ['black', 'white']
+sizes = ['S', 'M', 'L']
+tshirts = [(color, size) for color in colors for size in sizes]
+print(tshirts)
+
+
+
+
+
+
+def deco(function):
+    def inner():
+        print("running inner!")
+    return inner
+
+@deco
+def target():
+    print("running target!")
+
+target()
+
+v1 = Vector2d(3,4)
+print(abs(v1))
+print(hash(v1))
